116019cc54555e4488f1f4ea818bcb93e24a498c
Laptop <jeremiahlusajo96@gmail.com>
2023-11-02 12:08:35 +0100

    First Time

---
+++ src/com/example/ex1/Task.java
@@ -0,0 +1,8 @@
+package com.example.ex1;
+
+public class Task {
+    public static void main(String[] args) {
+        System.out.println("Hello Vistula!!");
+        System.out.println("My name is Jeremiah Lusajo.");
+    }
+}
